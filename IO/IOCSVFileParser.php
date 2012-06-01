b3f63ce755fccf75065974a34ea8a8a2afd342be
Luke Mills <luke@aligent.com.au>
2012-06-01 09:48:14 +0930

    Some fleshing out of IOCSVFipeParser

--- IO/IOCSVFileParser.php
+++ IO/IOCSVFileParser.php
@@ -7,14 +7,57 @@ require_once 'IOFileParserInterface.php';
  * 
  */
 class IOCSVFileIterator implements IOFileParserInterface {
+    
+    /**
+     *
+     * @var resource
+     */
+    private $handle = NULL;
+    
+    /**
+     *
+     * @var int
+     */
+    private $length = 0;
+    
+    /**
+     *
+     * @var char
+     */
+    private $delimiter = '';
+    
+    /**
+     *
+     * @var char
+     */
+    private $enclosure = '';
+    
+    /**
+     *
+     * @var char
+     */
+    private $escape = '';
 
-    public function __construct($handle) {
+    /**
+     *
+     * @param resource  $handle
+     * @param int       $length
+     * @param char      $delimiter
+     * @param char      $enclosure
+     * @param char      $escape
+     */
+    public function __construct($handle, $length = 0, $delimiter = ',', $enclosure = '"', $escape = '\\') {
         throw new IONotImplementedYetException();
-        parent::__construct($handle);
+        
     }
 
+    /**
+     *
+     * @return array|NULL|FALSE
+     */
     public function readLine() {
         throw new IONotImplementedYetException();
+        return fgetcsv($this->handle, $this->length, $this->delimiter, $this->enclosure, $this->escape);
     }
 
 }
