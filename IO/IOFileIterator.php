07a0dd6d48d0b41e141c12a86ea6258cc297cf76
Jonathan <jonathan@aligent.com.au>
2013-05-20 12:27:56 +0930

    minor code formatting fix

--- IO/IOFileIterator.php
+++ IO/IOFileIterator.php
@@ -111,7 +111,8 @@ abstract class IOFileIterator implements IOIteratorInterface {
             $this->_fieldProcessor = $fieldProperties;
         } else {
             $this->_fieldProcessor = new IOFieldProcessor((array) $fieldProperties);
-        }        $this->hasHeaderRow = $hasHeaderRow;
+        }
+        $this->hasHeaderRow = $hasHeaderRow;
         $isHeaderMatch = $this->isHeaderMatch();
         $this->rewind();
         return $isHeaderMatch;
